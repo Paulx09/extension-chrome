6234dfcac6fd7d739fb7abe950241be2ff34f36c
PaulxA09 <pauloarmey18@gmail.com>
2025-02-19 23:35:16 -0500

    Actualizar versión a 0.0.3 y agregar interacciones de usuario en scripts

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
-setTimeout(() => {
-    alert("Hola, bienvenido a mi extensión.")
-}, 3000)
+function hideElement(event) {
+    const element = event.target
+
+    element.style.display = "none"
+}
+
+function addBorder(event) {
+    const element = event.target
+
+    element.style.border = "2px solid darkred"
+}
+
+function removeBorder(event) {
+    const element = event.target
+
+    element.style.border = ""
+}
+
+document.body.addEventListener("click", hideElement)
+document.body.addEventListener("mouseover", addBorder)
+document.body.addEventListener("mouseout", removeBorder)
