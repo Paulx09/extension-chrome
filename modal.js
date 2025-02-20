1184a4c55183a929f286bc04058e60a687d95224
PaulxA09 <pauloarmey18@gmail.com>
2025-02-20 00:36:11 -0500

    Actualizar versión a 0.0.5, agregar retraso para mostrar modal y mejorar estilos

--- modal.js
+++ modal.js
@@ -2,4 +2,7 @@ const modal = document.createElement("div")
 modal.classList.add("show")
 modal.innerHTML = "<h2>Vuelve al trabajo</h2><p>Acabó tu tiempo</p>"
 
-document.body.appendChild(modal)
+window.setTimeout(() => {
+    document.body.appendChild(modal)
+    document.body.style.overflow = "hidden"
+}, 10000)
